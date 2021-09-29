51911b73d4a514ede172b4c17df1a9763f7f78ef
vwno <79748620+JAIGANESH-TS@users.noreply.github.com>
2021-09-29 10:04:18 +0530

    Create crosspattern.cpp

---
+++ crosspattern.cpp
@@ -0,0 +1,21 @@
+class Solution{   
+public:
+    string crossPattern(string s){
+        // code here 
+        string ans="";
+       int l=s.length();
+       for(int i=0;i<l;i++)
+       {
+           for(int j=0;j<l;j++)
+           {
+               if(i==j)
+               ans+=s[i];
+               else if(i+j==l-1)
+               ans+=s[j];
+               else
+               ans+=" ";
+           }
+       }
+       return ans;
+    }
+};
